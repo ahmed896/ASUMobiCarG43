f573938a83fdccc2c85461a1a00ceb6c748981be
ahmed896 <36419436+ahmed896@users.noreply.github.com>
2018-02-20 22:10:11 +0200

    Update example_2.ino

--- example2/example_2.ino
+++ example2/example_2.ino
@@ -6,8 +6,9 @@ pinMode(led3,OUTPUT);
 pinMode(led4,OUTPUT);}
   void loop () {
     for (int i=4;i<8;i++){
-   digitalWrite(i, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(i, LOW);    // turn the LED off by making the voltage LOW
-      delay(1000);             }          // wait for a second
-}
+   digitalWrite(i, HIGH);  
+  delay(1000);                      
+  digitalWrite(i, LOW);      
+
+      delay(1000);             }      
+  }
