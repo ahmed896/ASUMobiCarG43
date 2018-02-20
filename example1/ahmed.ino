8ce08e37837bf8663a9b3b4d5e8ce39f69cdf782
ahmed896 <36419436+ahmed896@users.noreply.github.com>
2018-02-20 22:08:55 +0200

    Update ahmed.ino

--- example1/ahmed.ino
+++ example1/ahmed.ino
@@ -3,8 +3,8 @@ void setup () {
   pinMode(led,OUTPUT);}
   void loop () {
     
-   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+   digitalWrite(led, HIGH);   
+  delay(1000);                      
+  digitalWrite(led, LOW);    
+  delay(1000);                     
 }
