3efd982ea81ff8ff5a8f75e4023b77687f309db4
Abanoub Emil <36601675+abanoubemil@users.noreply.github.com>
2018-02-23 00:00:47 +0200

    Add files via upload

---
+++ project sheet 1/ex_2 edit.ino
@@ -0,0 +1,24 @@
+void setup() {
+pinMode (0,OUTPUT);
+pinMode (1,OUTPUT);
+pinMode (2,OUTPUT);
+pinMode (3,OUTPUT);
+}
+
+void loop() {
+for(int i=0;i<=3;i++)
+{
+  digitalWrite(i,HIGH);
+  delay(1000);
+  digitalWrite(i,LOW);
+  if (i==3)
+  {
+    for (i=2;i>=0;i--)
+    {
+      digitalWrite(i,HIGH);
+      delay(1000);
+      digitalWrite(i,LOW);
+    }
+  }
+}
+}
