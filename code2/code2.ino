68a5bd472f5dbdf115ff48806a4b2197be97d4d6
a1h2med <31229408+a1h2med@users.noreply.github.com>
2018-02-23 10:28:04 +0200

    Add files via upload

---
+++ code2/code2.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+for (int i = 4 ; i<=7 ; i++)
+{
+  pinMode(i,OUTPUT);
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for (int i = 4 ; i<=7 ; i++)
+{
+  digitalWrite(i,HIGH);
+}
+delay (1000);
+for (int i = 7 ; i>=4 ; i--)
+{
+  digitalWrite(i,LOW);
+}
+}
