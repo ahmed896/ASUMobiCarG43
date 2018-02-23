8316f9092d5e6798cbdc9733b8679bbfdda2e3ad
a1h2med <31229408+a1h2med@users.noreply.github.com>
2018-02-23 10:37:14 +0200

    Add files via upload

---
+++ code 3/code 3.ino
@@ -0,0 +1,14 @@
+int x=5;
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("your number is : ");
+  Serial.println(x);
+  x++;
+ delay(500);
+}
