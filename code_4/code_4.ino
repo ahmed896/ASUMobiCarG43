45c489904ae0a7c8e0a5a561c883b61343768604
a1h2med <31229408+a1h2med@users.noreply.github.com>
2018-02-25 01:29:28 +0200

    Add files via upload

---
+++ code_4/code_4.ino
@@ -0,0 +1,43 @@
+byte pot = A0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  short potReading = analogRead (pot);
+  if (potReading <= 250)
+  { digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+  }
+  else if (potReading <= 500 && potReading > 250)
+  { digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(10, LOW);
+  }
+  else  if (potReading <= 750 && potReading > 500)
+  { digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(10, LOW);
+  }
+  else if (potReading >= 750)
+  { digitalWrite(10, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
+  }
+
+  Serial.print(potReading);
+  Serial.println();
+  delay(20);
+
+}
