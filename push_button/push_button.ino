5c8c916afee891da7aa5e45f859781a07378b8dd
AhmedElMorsy97 <36449244+AhmedElMorsy97@users.noreply.github.com>
2018-02-25 22:23:47 +0200

    Add files via upload

---
+++ push_button/push_button.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(12,INPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int value = digitalRead(12);
+digitalWrite(9,value);
+}
