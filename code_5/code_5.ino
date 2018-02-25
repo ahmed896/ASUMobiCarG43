e1995ec1c3ffc65171e1c48e1a5021eea87fb68e
a1h2med <31229408+a1h2med@users.noreply.github.com>
2018-02-25 22:33:10 +0200

    Add files via upload

---
+++ code_5/code_5.ino
@@ -0,0 +1,13 @@
+int motorPin1=10;
+int motorPin2=9;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(motorPin1,OUTPUT);
+pinMode(motorPin2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(motorPin1,HIGH);
+digitalWrite(motorPin2,LOW);
+}
