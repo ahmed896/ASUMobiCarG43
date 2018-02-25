cf36e2c6e386be5d7b993a84ff669dc5ac325be0
AhmedElMorsy97 <36449244+AhmedElMorsy97@users.noreply.github.com>
2018-02-25 22:24:27 +0200

    Add files via upload

---
+++ pot/pot.ino
@@ -0,0 +1,13 @@
+void setup()
+{  
+  pinMode(13,OUTPUT);
+}
+void loop()
+{   
+  int ledDelay = analogRead(A0);
+    digitalWrite(13,HIGH);
+    delay(ledDelay);
+    digitalWrite(13,LOW);
+    delay(ledDelay);  
+}
+
