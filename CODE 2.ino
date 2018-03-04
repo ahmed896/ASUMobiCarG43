d653eeb48cda7949d5a77f07f537fa2960ef32e3
ahmed islam <36420417+ahmedeslam@users.noreply.github.com>
2018-03-04 19:15:26 +0200

    Add files via upload

---
+++ CODE 2.ino
@@ -0,0 +1,26 @@
+int led1=4;
+int led2=5;
+int led3=6;
+int led4=7;
+void setup() {
+ pinMode(led1,OUTPUT);
+ pinMode(led2,OUTPUT);
+ pinMode(led3,OUTPUT);
+ pinMode(led4,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1,HIGH);
+  delay(1000);
+  digitalWrite(led2,HIGH);
+  delay(1000);
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led4,HIGH);
+  delay(1000);
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led2,HIGH);
+  delay(1000);
+    
+}
