cdbe25ac682c27d3919357b6f3a7e32aa2bdf5f8
ahmed islam <36420417+ahmedeslam@users.noreply.github.com>
2018-03-27 04:24:33 +0200

    Add files via upload

---
+++ sketch_mar26a.ino
@@ -0,0 +1,49 @@
+int RS=2;
+int LS=3;
+int LM1=4;
+int LM2=5;
+int RM1=6;
+int RM2=7;
+
+void setup() {
+  pinMode(LM1,OUTPUT);
+  pinMode(LM2,OUTPUT);
+  pinMode(RM1,OUTPUT);
+  pinMode(RM2,OUTPUT);
+  pinMode(RS,INPUT) ;
+  pinMode(LS,INPUT) ;
+}
+
+void loop() {
+   if(digitalRead(LS) && digitalRead(RS))     //FOrward
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && digitalRead(RS))     //right
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))     //left
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     //stop
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+}
