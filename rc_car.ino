a246e15de84d70401b6c3fcad15945f775f3557e
a1h2med <31229408+a1h2med@users.noreply.github.com>
2018-04-02 22:01:02 +0200

    Add files via upload

---
+++ rc_car.ino
@@ -0,0 +1,65 @@
+byte motor1 = 3, motor2 = 5, motor3 = 6, motor4 =9 , echo = 10 , trig = 11;
+float take =0 ,give = 0 ;
+char state; 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+pinMode(motor1,OUTPUT);
+pinMode(motor2,OUTPUT);
+pinMode(motor3,OUTPUT);
+pinMode(motor4,OUTPUT);
+pinMode(echo,INPUT);
+pinMode(trig,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trig, LOW);
+  take = pulseIn(echo, HIGH);
+  give = (take * .034) / 2;
+  if(give<=20)
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,LOW); 
+  }
+  else
+  {
+while(Serial.available())
+{
+  state = Serial.read();
+  Serial.println(state);
+  if (state == 'F')
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,HIGH);
+    digitalWrite(motor4,LOW);
+  }
+  if (state == 'B')  // move back
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,HIGH);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,HIGH);
+  }
+  if (state == 'L') // move left
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,LOW);
+  }
+  if (state == 'R') // move right
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,HIGH);
+    digitalWrite(motor4,LOW);
+  }
+}
+}
+}
