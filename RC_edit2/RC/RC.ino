e6477b29337946ac4ba513d7f8c56c9d7c9ae307
ahmedashraf <31229408+a1h2med@users.noreply.github.com>
2018-07-03 05:58:31 +0200

    Add files via upload

---
+++ RC_edit2/RC/RC.ino
@@ -0,0 +1,279 @@
+#include <stdlib.h>
+byte motor1 = 3, motor2 = 5, motor3 = 6, motor4 =9 , echo = 10 , trig = 11 ,x=0,RS=7, LS=2, MS =4, left = 0, right = 0 , med = 0,pr=1,pl=1,pm=1 ,  d = 0;
+float take =0 ,give = 0 ;long value[4];
+char state,current ;
+char distance[4];
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(motor1,OUTPUT);
+pinMode(motor2,OUTPUT);
+pinMode(motor3,OUTPUT);
+pinMode(motor4,OUTPUT);
+pinMode(echo,INPUT);
+pinMode(RS,INPUT) ;
+pinMode(LS,INPUT) ;
+pinMode(MS,INPUT) ;
+pinMode(trig,OUTPUT);
+}
+void Forward ()
+{
+  digitalWrite(motor1,LOW);
+    analogWrite(motor2,127);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,127);
+}
+void Back ()
+{
+    digitalWrite(motor2,LOW);
+    analogWrite(motor1,127);
+    digitalWrite(motor4,LOW);
+    analogWrite(motor3,127);
+}
+void forward ()  
+{                
+    digitalWrite(motor1,LOW);
+    analogWrite(motor2,100);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,100);
+}
+void turnLeft ()
+{
+    analogWrite(motor1,0);
+    analogWrite(motor2,200);
+    analogWrite(motor3,200);
+    digitalWrite(motor4,0);
+}
+void turnRight ()
+{
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,200);
+}
+void turnLeftSmooth ()
+{
+    analogWrite(motor1,150);
+    analogWrite(motor2,0);
+    analogWrite(motor3,75);
+    analogWrite(motor4,0);
+}
+void turnRightSmooth ()
+{
+    analogWrite(motor1,0);
+    analogWrite(motor2,150);
+    analogWrite(motor3,0);
+    analogWrite(motor4,75);
+}
+void Stop ()
+{
+    digitalWrite(motor1,LOW);
+    analogWrite(motor2,0);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,0);
+}
+void wait()
+{
+  turnRight();
+}
+int reading ()
+{
+  int val = 0;
+      val=analogRead(0);   //connect mic sensor to Analog 0
+      if (val <= 466 && val >=422 )
+      val = 0;
+      if (val != 0)
+      x++;
+      delay(100);
+      return x;
+      if (x ==45)
+      x = 0;
+      Serial.println(x);
+}
+void back()
+{
+  digitalWrite(motor2,LOW);
+    analogWrite(motor1,100);
+    digitalWrite(motor4,LOW);
+    analogWrite(motor3,100);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trig, LOW);
+  take = pulseIn(echo, HIGH);
+  give = (take * .034) / 2;
+  if(give<=20)
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,LOW); 
+    delay(1000);
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,HIGH);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,HIGH);
+    delay(200);
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,HIGH);
+    delay(500);
+    x=1;
+  }
+  else
+  {
+    if(x==1)
+    {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,HIGH);
+    digitalWrite(motor4,LOW);
+   }
+while(Serial.available())
+{
+  state = Serial.read();
+  Serial.println(state);
+  if (state == 'F')
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,HIGH);
+    digitalWrite(motor4,LOW);
+  }
+  if (state == 'B')  // move back
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,HIGH);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,HIGH);
+  }
+  if (state == 'L') // move left
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    digitalWrite(motor4,LOW);
+  }
+  if (state == 'R') // move right
+  {
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,HIGH);
+    digitalWrite(motor4,LOW);
+ }
+  if (state == 'W')
+  goto  AA;
+}
+  if (state == 'Z') // dh character morsy w islam hy3mloh 34an a5o4 f l accurate mode
+  goto BB;
+}
+AA :
+ left = digitalRead(LS);
+  right = digitalRead(RS);
+  med = digitalRead(MS);
+  if ( (med == 0 ) && (left == 1) && (right == 1))
+  forward();
+  if ( (med == 0) && (left == 0) && (right == 1))
+  turnRightSmooth();
+  if ( (med == 0 ) && (left == 1) && (right == 0))
+  turnLeftSmooth();
+  if ( (med == 1) && (right == 1) && (left == 0))
+  {
+    while (1)
+    {
+  turnLeft();
+  if ( (digitalRead(MS) == 0) || (digitalRead(RS) == 0 ))
+  break;
+    }
+  }
+  if ( (med == 1) && (right == 0) && (left == 1))
+  {
+    while (1)
+    {
+  turnRight();
+  if ( (digitalRead(MS) == 0) || (digitalRead(LS) == 0))
+  break;
+  }
+  }
+  if ( (med == 1) && ( right == 1) && (left == 1))
+  {
+    wait();
+   // Stop(); 
+  }
+  //       here will be the features of accurate movement code
+  BB :
+  while (Serial.available())
+  {
+    float timer = 0;int number = 0,result=0;
+    current = Serial.read();
+    for (int i=0;i<4;i++)      // hena h5zn l distance 
+    {
+    distance[i] = Serial.read();
+    value[i] = atol(distance);
+    number++;
+    if (distance[i]=='\0')
+    break;
+    }
+    for (int i=0;i<number;i++)    // hena b7awl a5ly l array kloh fe variable wa7d
+    {
+      if (i==0)
+      result+=value[i];
+      else{
+        result = result * 10;
+        result+=value[i];
+      }
+    }
+
+   if ( (result%10) == 0)
+   {
+    int see = result/10;
+     while (1)
+     {
+      forward();
+      d = reading();
+      if ( d == see )
+      {
+      Stop();
+      x = 0;
+      break;
+      } } }
+      else {
+        int see = result/10;
+        while (1)
+     {
+      forward();
+      d = reading();
+      if ( d == see )
+      { delay(50);
+      Stop();
+      x = 0;
+      break;
+      } } }
+      
+   /* if (current == 'z') // dh l character ly hygyly wly 
+    {
+      while (1)
+      {
+      timer = millis()*10^(-3);
+      Forward();
+       if (( .48 * timer ) <= result ) // l mfrod a3ml relation ben l sor3a wl time 34an azbt l distance
+     {  Stop(); break;}
+      }
+   }*/
+ /* else if (current == 'y') // dh l char ly morsy we eslam hyb3toh
+   {
+    while (1)
+    {
+    timer = millis()*10^(-3);
+    Back();
+    if ((.48 * timer ) <= result ) // l mfrod a3ml relation ben l sor3a wl time 34an azbt l distance
+    {
+    Stop();break;
+    }
+    }
+   }*/
+  }
+} 
---
+++ Rectangle/Rectangle.ino
@@ -0,0 +1,59 @@
+byte motor1 = 3, motor2 = 5, motor3 = 6, motor4 =9 , echo = 10 , trig = 11 ,x=0,RS=7, LS=2, MS =4, left = 0, right = 0 , med = 0,pr=1,pl=1,pm=1 , SoundSensor = 8;
+float take =0 ,give = 0 ;int counter = 0 , RightSmooth =0, Right = 0 , LeftSmooth = 0 , Left = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+pinMode(motor1,OUTPUT);
+pinMode(motor2,OUTPUT);
+pinMode(motor3,OUTPUT);
+pinMode(motor4,OUTPUT);
+pinMode(echo,INPUT);
+pinMode(RS,INPUT) ;
+pinMode(LS,INPUT) ;
+pinMode(MS,INPUT) ;
+pinMode(SoundSensor,INPUT);
+pinMode(trig,OUTPUT);
+}
+void forward ()  
+{                
+    digitalWrite(motor1,LOW);
+    analogWrite(motor2,100);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,100);
+}
+void turnRight ()
+{
+    digitalWrite(motor1,LOW);
+    digitalWrite(motor2,LOW);
+    digitalWrite(motor3,LOW);
+    analogWrite(motor4,200);
+}
+int reading ()
+{
+  int val = 0;
+      val=analogRead(0);   //connect mic sensor to Analog 0
+      if (val <= 466 && val >=422 )
+      val = 0;
+      if (val != 0)
+      x++;
+      delay(100);
+      return x;
+      if (x ==45)
+      x = 0;
+      Serial.println(x);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+while (1) 
+  {
+    forward();
+      int y = 0;
+      y = reading();
+      if (y == 45)
+      {
+        turnRight ();
+        delay(400);
+        x = 0;
+      }
+  }
+}
