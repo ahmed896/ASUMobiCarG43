9a818fa22edb5d88921754940989b22641b9062b
ahmedashraf <31229408+a1h2med@users.noreply.github.com>
2018-07-04 07:52:23 +0200

    Add files via upload

---
+++ circle_Last_edit/circle.ino
@@ -0,0 +1,22 @@
+byte motor1 = 3, motor2 = 5, motor3 = 6, motor4 =9 , echo = 10 , trig = 11 ,x=0,RS=7, LS=2, MS =4, left = 0, right = 0 , med = 0,pr=1,pl=1,pm=1 , SoundSensor = 8;
+float take =0 ,give = 0 ;int counter = 0 , RightSmooth =0, Right = 0 , LeftSmooth = 0 , Left = 0;
+void setup() {
+  byte motor1 = 3, motor2 = 5, motor3 = 6, motor4 =9;
+pinMode(motor1,OUTPUT);
+pinMode(motor2,OUTPUT);
+pinMode(motor3,OUTPUT);
+pinMode(motor4,OUTPUT);
+}
+void circle()
+{
+  analogWrite(motor1,0);
+  analogWrite(motor2,150);
+  analogWrite(motor3,0);
+  analogWrite(motor4,50);
+  //this values change to get the redious required which is equal to 1 m 
+  }
+
+void loop() {
+  circle();
+ 
+}
